2b83e52333de5085139d5cb9e7705b65da155b9f
petrusgunder <petrus.fs2009@aluno.ifsc.edu.br>
2026-08-16 22:44:48 -0300

    commit do html css - que comece a revoluçaõ

--- site/app/routes/public.py
+++ site/app/routes/public.py
@@ -5,3 +5,7 @@ public_bp = Blueprint('public', __name__)
 @public_bp.route('/')
 def home():
     return render_template('base.html')
+
+@public_bp.route('/home')
+def home_page():
+    return render_template('home.html')
