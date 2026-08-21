8e9f3ac73f31f6cd59adaff61c615a131737835b
petrusgunder <petrus.fs2009@aluno.ifsc.edu.br>
2026-08-21 15:22:36 -0300

    commit das paina tudo

--- src/app/routes/public.py
+++ src/app/routes/public.py
@@ -10,3 +10,15 @@ def home():
 @public_bp.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+@public_bp.route('/entrar')
+def loguin():
+    return render_template('login.html')
+
+@public_bp.route('/carrinho')
+def carrinho():
+    return render_template('favoritos.html')
+
+@public_bp.route('/menu_dos_menus')
+def menu_dos_menus():
+    return render_template('base_com_menu.html')
